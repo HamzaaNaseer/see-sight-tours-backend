29f7160d159eb785523c069b816a4f0523d91bb4
hamza naseer <hamzanaseer711@gmail.com>
2022-10-07 22:14:38 +0500

    test

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const connectToMongo = require("./config/db"); //function that upon calling will
 const dotenv = require("dotenv"); // for setting up enviroment variables
 //setting the path to the env file
 dotenv.config({ path: "config/config.env" });
-const port = 5000
 
 //calling function to connect to database
 connectToMongo();
 
 //creating a server
-app.listen(port, () => {
-  console.log("server is running at port ", port);
+app.listen(process.env.PORT, () => {
+  console.log("server is running at port ", process.env.port);
 });
