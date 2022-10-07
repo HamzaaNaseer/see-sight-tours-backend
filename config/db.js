2939bcd7d272617a8aed916c275d85427737b730
hamza naseer <hamzanaseer711@gmail.com>
2022-10-07 22:25:17 +0500

    ''

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //This function will be called to connect to our database
 const connectToMongo = () => {
-  mongoose.connect(process.env.DB_URI, () => {
+  mongoose.connect('mongodb://mongo:bksBdxjsThIRHLkH7UqK@containers-us-west-44.railway.app:5986', () => {
     console.log("connected to mongoose sucessfully");
   });
 };
