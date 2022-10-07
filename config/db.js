2ac32ac5aada2399bdde3521c5ac9de0f4cc7692
hamza naseer <hamzanaseer711@gmail.com>
2022-10-07 21:48:39 +0500

    few more testss

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+const uri = "mongodb+srv://hamza:t9dbVXbUY2EH6hp8@cluster0.rwvbdxm.mongodb.net/?retryWrites=true&w=majority"
 //This function will be called to connect to our database
 const connectToMongo = () => {
-  mongoose.connect(process.env.DB_URI, () => {
+  mongoose.connect("mongodb+srv://hamza:t9dbVXbUY2EH6hp8@cluster0.rwvbdxm.mongodb.net/?retryWrites=true&w=majority", () => {
     console.log("connected to mongoose sucessfully");
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./index");
 const connectToMongo = require("./config/db"); //function that upon calling will connect to our DB.
 const dotenv = require("dotenv"); // for setting up enviroment variables
-
 //setting the path to the env file
 dotenv.config({ path: "config/config.env" });
+const port = 5000
 
 //calling function to connect to database
 connectToMongo();
 
 //creating a server
-app.listen(process.env.PORT, () => {
-  console.log("server is running at port ", process.env.port);
+app.listen(port, () => {
+  console.log("server is running at port ", port);
 });
