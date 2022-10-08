60cfa115d9cc141ccc356d6fd1a9be4871ab3c84
hamza naseer <hamzanaseer711@gmail.com>
2022-10-08 15:35:13 +0500

    adding env var

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //This function will be called to connect to our database
 const connectToMongo = () => {
-  mongoose.connect('mongodb+srv://hamza:KRF9AGtc9J3Usp8E@cluster0.2c4zxgu.mongodb.net/?retryWrites=true&w=majority', () => {
+  mongoose.connect(process.env.DB_URI, () => {
     console.log("connected to mongoose sucessfully");
   });
 };
